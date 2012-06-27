c85ba7c1c99e3a0ab143bdf914794ac9081651bf
Huisache <Huisache@git>
2012-06-26 18:32:11 -0700

    day2

--- markov.py
+++ markov.py
@@ -1,36 +1,50 @@
-
+import random 
 
 def process_file(filename):
 	output = open(filename, "r")
-	line = output.readline()
+	line = output.read()
 	output.close()
-	line = line [0: -1]
-	words = line.split(" ")
+	words = line.split()
 	#print words
 	
 	markov = {}
-	
-	if len(line) > 3: 
-		prefix = (words[0], words[1]) 
-		suffix = [words[2]]
-		markov[prefix] = suffix
+
+ 
+	for i in range(len(words)-2): 
+		prefix = (words[i], words[i+1]) 
+		suffix = words[i+2]
+		# markov[prefix] = suffix
+
+		if prefix in markov.keys():
+			markov[prefix].append(suffix)
+		else:
+			markov[prefix] = [suffix]
 	
 	return markov
 
-def shift():	
-		# t = (words[0], words[1])
-		# tuple_shifted = process_file(words)
-		# print tuple_shifted
-	
+def shift(t, new_element):
+	return (t[1], new_element)
+
+def build_sentence(mdict): 
+	prefix = random.choice(mdict.keys())
+	suffix = mdict[prefix]
+	l = [prefix[0], prefix[1], suffix[0]]
+	sentence = " ".join(l)
+	return sentence
+
 
 def main():
-	#print process_file("sample2.txt")
+	mdict = process_file("sample2.txt")
+	print build_sentence(mdict)
+	#print mdict
 	pass
+
+
 
 #for bottom of file
 if __name__ == "__main__":
 		main()
 
 
-		# (a, b) + (c, )  
-		# (a, b, c )
+# 		# (a, b) + (c, )  
+# 		# (a, b, c )
